f36a3f3713f36c462ab353e287e4efc1b9ca3404
Ankush Bahekar <ankushmb9@gmail.com>
2022-03-24 15:03:14 +0530

    [Ankush] Add: UnOrdered List

---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DataStructure
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Program number to get executed \n1. Unordred list \n2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Unorderedlist unordered = new Unorderedlist();
+                        unordered.Operation();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ DataStructure/DataStructure/Unorderedlist.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class Unorderedlist
+    {
+        public void Operation()
+        {
+            string data = File.ReadAllText(@"C:\User\@nkush\DataType\Data Structure Programs Day16-17\Data_Structure_Programs\DataStructure\DataStructure\Unorderedlist.cs");
+            Console.WriteLine("-----------------------Data from the file..---------------------------");
+            Console.WriteLine(data);
+            string[] words = data.Split(' ');
+            LinkedList<string> linkedlist = new LinkedList<string>();
+            foreach (string item in words)
+            {
+                linkedlist.AddLast(item);
+            }
+            Console.WriteLine("data into array...");
+            Display(linkedlist);
+            Console.WriteLine("Enter the string you want to search");
+            string input = Console.ReadLine();
+            if (linkedlist.Contains(input))
+            {
+                linkedlist.Remove(input);
+                Console.WriteLine($"{input} removed from the list");
+            }
+            else
+            {
+                linkedlist.AddLast(input);
+                Console.WriteLine($"{input} added to the list");
+            }
+            Console.WriteLine("-----------Data from the file after changes-----------------");
+            Display(linkedlist);
+        }
+        private void Display(LinkedList<string> linkedlist)
+        {
+            foreach (var item in linkedlist)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
